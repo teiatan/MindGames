a1759dde7d23653da282a23f1cec0b323024acc7
teiatan <92535482+teiatan@users.noreply.github.com>
2023-05-31 12:02:09 +0300

    moove all screnns to specific folder

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { EnterNumberScreen } from './components/screens/EnterNumberScreen';
-import { MoreOrLessScreen } from './components/MoreOrLessScreen';
-import { ResultsScreen } from './components/screens/ResultsScreen';
+import { EnterNumberScreen } from './components/moreOrLess-game-screens/EnterNumberScreen';
+import { MoreOrLessScreen } from './components/moreOrLess-game-screens/MoreOrLessScreen';
+import { ResultsScreen } from './components/moreOrLess-game-screens/ResultsScreen';
 
 export default function App() {
   return (
