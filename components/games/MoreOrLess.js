1b5640570214ca625ebd984ab6104f6a551a2b93
teiatan <92535482+teiatan@users.noreply.github.com>
2023-05-31 21:34:26 +0300

    add play logic

--- components/games/MoreOrLess.js
+++ components/games/MoreOrLess.js
@@ -9,8 +9,9 @@ import { Results } from "../moreOrLess-game-screens/Results";
 export function MoreOrLess() {
 
     const [currentScreen, setCurrentScreen] = useState('StartSettings');
+    const [numbersRange, setNumbersRange] = useState({min: 0, max: 0});
     const [initialNumber, setInitialNumber] = useState('');
-    const [totalAnswers, setTotalanswers] = useState(0);
+    const [totalAnswers, setTotalAnswers] = useState(0);
     const [correctAnswers, setCorrectAnswers] = useState(0);
 
     const displayScreen = (screenName) => {
@@ -27,28 +28,45 @@ export function MoreOrLess() {
     const chooseNumbersGange = (min, max) => {
         const number = Math.floor(Math.random() * (max - min + 1) + min);
         setInitialNumber(number);
+        setNumbersRange({min, max});
     }
 
     return(
         <View style={styles.gameContainer}>
+
             {displayScreen('StartSettings') && 
                 <StartSettings 
                     changeScreen={changeScreen}
                     chooseNumbersGange={chooseNumbersGange}
                 />
             }
+
             {displayScreen('StartGame') && 
                 <StartGame 
                     changeScreen={changeScreen}
                     initialNumber={initialNumber}
                 />
             }
-            {displayScreen('Play') && <Play changeScreen={changeScreen}/>}
+
+            {displayScreen('Play') && 
+                <Play 
+                    changeScreen={changeScreen}
+                    correctAnswers={correctAnswers}
+                    totalAnswers={totalAnswers}
+                    setTotalAnswers={setTotalAnswers}
+                    setCorrectAnswers={setCorrectAnswers}
+                    initialNumber={initialNumber}
+                    min={numbersRange.min}
+                    max={numbersRange.max}
+                />
+            }
+
             {displayScreen('Results') && 
                 <Results 
                     changeScreen={changeScreen} 
                     correctAnswers={correctAnswers}
                     totalAnswers={totalAnswers}
+                    
                 />
             }
         </View>
--- components/moreOrLess-game-screens/Play.js
+++ components/moreOrLess-game-screens/Play.js
@@ -1,27 +1,59 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "../Button";
 import { colorsPalette } from "../../utils/colorsPalette";
+import { useState } from "react";
 
-export function Play({changeScreen}) {
+export function Play({changeScreen, correctAnswers, setCorrectAnswers, totalAnswers, setTotalAnswers, initialNumber, min, max}) {
+
+    const [currentNumber, setCurrentNumber] = useState(Math.floor(Math.random() * (max - min + 1) + min));
+    const [previousNumber, setPreviousNumber] = useState(initialNumber)
+    const [gameNumbers, setGameNumbers] = useState({prev: initialNumber, cur: Math.floor(Math.random() * (max - min + 1) + min)})
+
+    const setNewCurrentNumber = () => {
+        let number;
+        do {
+            number = Math.floor(Math.random() * (max - min + 1) + min)
+        } while (number === gameNumbers.cur);
+        return number;
+    };
+
+    const pressLess = () => {
+        setTotalAnswers(prev => prev+1);
+        if(gameNumbers.prev > gameNumbers.cur) {
+            setCorrectAnswers(prev => prev+1);
+        };
+        setGameNumbers(prev => ({prev: prev.cur, cur: setNewCurrentNumber()}))
+    };
+
+    const pressMore = () => {
+        setTotalAnswers(prev => prev+1);
+        if(gameNumbers.prev < gameNumbers.cur) {
+            setCorrectAnswers(prev => prev+1);
+        };
+        setGameNumbers(prev => ({prev: prev.cur, cur: setNewCurrentNumber()}))
+    };
 
     const finishGame = () => {
         changeScreen('Results');
-    }
+    };
 
     return(
         <View style={styles.container}>
+            <Text>Total: {totalAnswers} Correct: {correctAnswers}</Text>
             <Text style={styles.heading}>Is this number bigger or smaller than previous?</Text>
-            <Text style={styles.randomNumber}>45</Text>
+            <Text style={styles.randomNumber}>{gameNumbers.cur}</Text>
             <View style={styles.buttonsContainer}>
                 <Button 
                     text='smaller'
                     containerStyles={styles.lessButtonContainer}
                     textStyles={styles.lessButtonText}
+                    onPress={pressLess}
                 />
                 <Button 
                     text='bigger'
                     containerStyles={styles.moreButtonContainer}
                     textStyles={styles.moreButtonText}
+                    onPress={pressMore}
                 />
             </View>
             <Button 
